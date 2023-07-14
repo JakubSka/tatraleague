4daa3b0b81a2ce42bf15ca3ab22aad9cc54a2073
JakubSka <kuba.jarnice@gmail.com>
2023-07-14 16:41:24 +0200

    full scale view

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//hiding header
 window.addEventListener("scroll", function () {
   const leaderboard = document.querySelector(".leaderboard");
   const lbcoords = leaderboard.getBoundingClientRect();
